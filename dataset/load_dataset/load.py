6b9f2935a9ffefdc97cf54e760225514b4a71a44
martinasalis <marti.salis20@gmail.com>
2021-03-03 12:24:44 +0100

    Create new dataframe and ordered dataframe's schema

--- dataset/load_dataset/load.py
+++ dataset/load_dataset/load.py
@@ -1,11 +1,10 @@
-import json
+import pyspark.sql.functions as sf
 import os
 
 
 def load_dataset(spark, file_name):
 
     data = spark.read.json(file_name, multiLine="true")
-    print(data.show(10, False))
 
     return data
 
@@ -15,7 +14,9 @@ def load_texts(spark, folder_name, data_info):
     #texts = os.listdir(folder_name)
     texts = ["1114679353714016256.json", "1063020048816660480.json", "1106978219654303744.json"]
 
-    texts_info = None
+    texts_info = spark.createDataFrame([], data_info.select("1035252480215592966.*").schema)
+    texts_info = texts_info.withColumn("id", sf.lit(''))
+    texts_info = texts_info.select('id', 'img_url', 'labels', 'labels_str', 'tweet_text', 'tweet_url')
 
     print("Start create DataFrame ...")
 
@@ -23,16 +24,13 @@ def load_texts(spark, folder_name, data_info):
 
         file_name = os.path.splitext(text)[0]
 
-        print("Read file: " + file_name)
+        #print("Read file: " + file_name)
 
-        text_info = data_info.select(file_name)
+        text_info = data_info.select(file_name + ".*")
 
-        if texts_info is None:
+        text_info = text_info.withColumn("id", sf.lit(file_name))
+        text_info = text_info.select('id', 'img_url', 'labels', 'labels_str', 'tweet_text', 'tweet_url')
 
-            texts_info = text_info
-
-        else:
-
-            texts_info = texts_info.union(text_info)
+        texts_info = texts_info.union(text_info)
 
     return texts_info
