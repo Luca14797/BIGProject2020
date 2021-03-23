d4d2d7e2b100a85e48629e07d1b05c953dc3d675
Luca14797 <37103375+Luca14797@users.noreply.github.com>
2021-03-23 23:28:05 +0100

    Modified files main and load

--- src/load_dataset/load.py
+++ src/load_dataset/load.py
@@ -1,5 +1,4 @@
 import pyspark.sql.functions as sf
-import os
 
 
 def load_dataset(spark, file_name):
@@ -11,50 +10,24 @@ def load_dataset(spark, file_name):
 
 def load_texts(spark, sc, base_path, data_info, split_name):
 
-    texts = os.listdir(base_path + '/texts/')
+    texts = sc.textFile(base_path + "/texts_list.txt")
     texts_split = sc.textFile(base_path + '/splits/' + split_name + '.txt').collect()
 
-    '''
-    texts = ["1114679353714016256.json", "1063020048816660480.json", "1108927368075374593.json",
-             "1114558534635618305.json", "1035252480215592966.json", "1106978219654303744.json",
-             "1113920043568463874.json", "1114588617693966336.json", "1045809514740666370.json",
-             "1047321356763844608.json", "1107433335318609924.json", "1055298801056014338.json",
-             "1110268446623875072.json", "1105839538050646023.json", "1037458903447945216.json",
-             "1109072018169839621.json", "1113224872870633472.json", "1114722908297945088.json",
-             "1116053548935069697.json", "1116887902859010054.json", "1105630895632011266.json",
-             "1115642566853242880.json", "1107491355704475648.json", "1116537005628780544.json",
-             "1052191770375835648.json", "1105524709029003267.json", "1105621285655203845.json",
-             "1107448953090392066.json", "1108400922198265857.json", "1113991085741826050.json",
-             "1115841044514516994.json", "1035111616331825152.json", "1114750222721323008.json",
-             "1107692206117318657.json", "1109878793462792193.json", "1058935000215838720.json",
-             "1042929715605061632.json", "1116482709474426880.json", "1110025135984005120.json",
-             "1053685584118583297.json", "1108208774639165440.json", "1113664242274664451.json",
-             "1061668739022815232.json", "1110336630659969024.json", "1114381633614950400.json",
-             "1108253229102624768.json", "1035521443927416832.json", "1037466084511830019.json"]
-    '''
-
     texts_info = spark.createDataFrame([], data_info.select(texts_split[0] + '.*').schema)
-    #texts_info = spark.createDataFrame([], data_info.select("1114679353714016256.*").schema)
     texts_info = texts_info.withColumn("id", sf.lit(''))
     texts_info = texts_info.select('id', 'img_url', 'labels', 'labels_str', 'tweet_text', 'tweet_url')
 
     print("Start create DataFrame ...")
 
-    #for text in texts:
-    for i in range(len(texts)//2):
+    texts_list = texts.filter(lambda x: x in texts_split).collect()
 
-        file_name = os.path.splitext(texts[i])[0]
+    for text in texts_list:
 
-        if file_name in texts_split:
+        text_info = data_info.select(text + ".*")
 
-            #print("Read file: " + file_name)
+        text_info = text_info.withColumn("id", sf.lit(text))
+        text_info = text_info.select('id', 'img_url', 'labels', 'labels_str', 'tweet_text', 'tweet_url')
 
-            text_info = data_info.select(file_name + ".*")
-
-            text_info = text_info.withColumn("id", sf.lit(file_name))
-            text_info = text_info.select('id', 'img_url', 'labels', 'labels_str', 'tweet_text', 'tweet_url')
-
-            texts_info = texts_info.union(text_info)
+        texts_info = texts_info.union(text_info)
 
     return texts_info
-
--- src/main.py
+++ src/main.py
@@ -7,26 +7,24 @@ from pyspark.ml.classification import LogisticRegression
 
 def main():
 
-    print("Create Spark Context")
-    #conf = SparkConf().setMaster("spark://DESKTOP-HTII7QG.localdomain:7077").setAppName("My App")
-    conf = SparkConf().setMaster("local").setAppName("My App")
-    sc = SparkContext(conf=conf)
+    print("Create Spark Context ...")
+    conf = SparkConf().setAppName("Big Data project")
+    sc = SparkContext.getOrCreate(conf=conf)
 
-    '''
-    spark = SparkSession.builder.master("spark://DESKTOP-HTII7QG.localdomain:7077").appName("My App")\
-        .config("spark.some.config.option", "some-value").getOrCreate()
-    '''
-    spark = SparkSession.builder.master("local").appName("My App").config("spark.some.config.option", "some-value")\
+    print("Create Spark Session ...")
+    spark = SparkSession.builder.appName("Big Data project").config("spark.some.config.option", "some-value")\
         .getOrCreate()
 
-    print("Load dataset")
+    print("Load Dataset ...")
     data_info = load.load_dataset(spark=spark, file_name="../dataset/info_texts.json")
     dataset = load.load_texts(spark=spark, sc=sc, base_path="../dataset", data_info=data_info, split_name='train')
 
+    print("Start Logistic Regression ...")
     tokenizer, hashingTF, idf = extract_features.tf_idf("tweet_text")
     label_stringIdx, dataset = extract_features.transform_labels(dataset)
     dataset = extract_features.create_pipeline(tokenizer, hashingTF, idf, label_stringIdx, dataset)
 
+    print("Logistic Regression ...")
     (trainingData, testData) = dataset.randomSplit([0.7, 0.3], seed=100)
     lr = LogisticRegression(maxIter=20, regParam=0.3, elasticNetParam=0)
     lrModel = lr.fit(trainingData)
