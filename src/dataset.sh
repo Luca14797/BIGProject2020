d532f60562e04e9f42bdd6ac8423d4247423a493
Luca14797 <37103375+Luca14797@users.noreply.github.com>
2021-06-08 19:48:30 +0200

    Update bash file

--- src/dataset.sh
+++ src/dataset.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-mkdir ../dataset
-mkdir ../dataset/split
+sudo mkdir /home/ubuntu/BIGProject2020/dataset
+sudo mkdir /home/ubuntu/BIGProject2020/dataset/split
 
-wget https://s3-terraform-bucket-big.s3.amazonaws.com/train.txt -P ../dataset/split
-wget https://s3-terraform-bucket-big.s3.amazonaws.com/test.txt -P ../dataset/split
-wget https://s3-terraform-bucket-big.s3.amazonaws.com/val.txt -P ../dataset/split
-wget https://s3-terraform-bucket-big.s3.amazonaws.com/info_texts.json -P ../dataset
-wget https://s3-terraform-bucket-big.s3.amazonaws.com/texts_list.txt -P ../dataset
+wget https://s3-terraform-bucket-big.s3.amazonaws.com/train.txt -P /home/ubuntu/BIGProject2020/dataset/split
+wget https://s3-terraform-bucket-big.s3.amazonaws.com/test.txt -P /home/ubuntu/BIGProject2020/dataset/split
+wget https://s3-terraform-bucket-big.s3.amazonaws.com/val.txt -P /home/ubuntu/BIGProject2020/dataset/split
+wget https://s3-terraform-bucket-big.s3.amazonaws.com/info_texts.json -P /home/ubuntu/BIGProject2020/dataset
+wget https://s3-terraform-bucket-big.s3.amazonaws.com/texts_list.txt -P /home/ubuntu/BIGProject2020/dataset
