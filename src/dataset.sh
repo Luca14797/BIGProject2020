c3489c128506657fa68867ee8b1e4933014979a1
Luca14797 <37103375+Luca14797@users.noreply.github.com>
2021-06-09 12:00:53 +0200

    Fixed bug

--- src/dataset.sh
+++ src/dataset.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 mkdir /home/ubuntu/BIGProject2020/dataset
-mkdir /home/ubuntu/BIGProject2020/dataset/split
+mkdir /home/ubuntu/BIGProject2020/dataset/splits
 
-wget https://s3-terraform-bucket-big.s3.amazonaws.com/train.txt -P /home/ubuntu/BIGProject2020/dataset/split
-wget https://s3-terraform-bucket-big.s3.amazonaws.com/test.txt -P /home/ubuntu/BIGProject2020/dataset/split
-wget https://s3-terraform-bucket-big.s3.amazonaws.com/val.txt -P /home/ubuntu/BIGProject2020/dataset/split
+wget https://s3-terraform-bucket-big.s3.amazonaws.com/train.txt -P /home/ubuntu/BIGProject2020/dataset/splits
+wget https://s3-terraform-bucket-big.s3.amazonaws.com/test.txt -P /home/ubuntu/BIGProject2020/dataset/splits
+wget https://s3-terraform-bucket-big.s3.amazonaws.com/val.txt -P /home/ubuntu/BIGProject2020/dataset/splits
 wget https://s3-terraform-bucket-big.s3.amazonaws.com/info_texts.json -P /home/ubuntu/BIGProject2020/dataset
 wget https://s3-terraform-bucket-big.s3.amazonaws.com/texts_list.txt -P /home/ubuntu/BIGProject2020/dataset
