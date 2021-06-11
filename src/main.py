9b77a2473a46271e538354ea07d0ecd93ce93e49
Luca14797 <37103375+Luca14797@users.noreply.github.com>
2021-06-11 17:25:35 +0200

    Insert test mode

--- src/main.py
+++ src/main.py
@@ -2,7 +2,7 @@ from pyspark import SparkContext, SparkConf
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import udf
 from pyspark.sql.types import IntegerType
-from pyspark.ml.classification import MultilayerPerceptronClassifier
+from pyspark.ml.classification import MultilayerPerceptronClassifier, LogisticRegression, NaiveBayes, LinearSVC
 from pyspark.ml.evaluation import MulticlassClassificationEvaluator
 from pyspark.ml.feature import HashingTF, IDF, RegexTokenizer
 from pyspark.ml import Pipeline
@@ -34,31 +34,9 @@ def transform_labels_majority(dataset):
     # 	      0 - NotHate, 1 - Racist, 2 - Sexist, 3 - Homophobe, 4 - Religion, 5 - OtherHate
     # For this project only two labels were considered: Hate and NotHate
     # To determine the label, the three labels of each tweet were checked
-    to_single_label = udf(lambda x: 0 if x.count(0) > 0 else 1, IntegerType())
+    to_single_label = udf(lambda x: 0 if x.count(0) > 1 else 1, IntegerType())
 
     dataset = dataset.withColumn("label", to_single_label(dataset.labels))
-
-    return dataset
-
-
-# This function create a single label for each tweet texts
-def transform_labels_person(dataset, person):
-
-    # Each one of the 150,000 tweets is labeled by 3 different workers using AMT
-    # labels: array with 3 numeric labels [0-5] indicating the label by each one of the three AMT annotators
-    # 	      0 - NotHate, 1 - Racist, 2 - Sexist, 3 - Homophobe, 4 - Religion, 5 - OtherHate
-    # For this project only two labels were considered: Hate and NotHate
-    # To determine the label, the label in position 'col' of each tweet is checked
-
-    if person == 3:
-        to_single_label = udf(lambda x: 0 if x[0] == 0 else 1, IntegerType())
-
-        dataset = dataset.withColumn("label", to_single_label(dataset.labels))
-
-    else:
-        to_single_label = udf(lambda x: 0 if x[len(x) - person] == 0 else 1, IntegerType())
-
-        dataset = dataset.withColumn("label", to_single_label(dataset.labels))
 
     return dataset
 
@@ -94,17 +72,8 @@ def load_texts(sc, base_path, data_info, split_name, partitions):
 
 def main():
 
-    # This flag indicate if the label is formed by using all three labels included in the dataset (majority)
-    # or if the label il formed selecting one of the labels included in the dataset (person)
-    # If this flag is set to True the label is majority
-    # If this flag is set to False one of the labels il selected
-    majority = True
-
-    # This flag indicate which of the three labels of the dataset is selected
-    # 3 -> first person
-    # 2 -> second person
-    # 1 -> third person
-    person = 3
+    # Flag
+    testMode = False
 
     print("Create Spark Context ...")
     conf = SparkConf().setAll([("spark.app.name", "Big Data project"),
@@ -119,53 +88,55 @@ def main():
     partitions = (sc.defaultParallelism * 2)  # (numClusterCores * replicationFactor)
 
     print("Load Dataset ...")
-    dataset = load_dataset(sc=sc, file_name="dataset/info_texts.json", partitions=partitions)
+    dataset = load_dataset(sc=sc, file_name="../dataset/info_texts.json", partitions=partitions)
 
     print("Split Dataset ...")
-    trainingData = load_texts(sc=sc, base_path="dataset", data_info=dataset, split_name='train', partitions=partitions)
-    testData = load_texts(sc=sc, base_path="dataset", data_info=dataset, split_name='test', partitions=partitions)
+    trainingData = load_texts(sc=sc, base_path="../dataset", data_info=dataset, split_name='train', partitions=partitions)
+    testData = load_texts(sc=sc, base_path="../dataset", data_info=dataset, split_name='test', partitions=partitions)
 
     print("Prepare Dataset ...")
     # Prepare test data
-    if majority:
-        testData = transform_labels_majority(dataset=testData)
-
-    else:
-        testData = transform_labels_person(dataset=testData, person=person)
+    testData = transform_labels_majority(dataset=testData)
 
     # Prepare training data
-    if majority:
-        trainingData = transform_labels_majority(dataset=trainingData)
-
-    else:
-        trainingData = transform_labels_person(dataset=trainingData, person=person)
+    trainingData = transform_labels_majority(dataset=trainingData)
 
     tokenizer, hashingTF, idf = tf_idf(col_name="tweet_text")
 
-    print("Prepare Multilayer Perceptron ...")
-    layers = [150, 64, 16, 2]
+    if testMode:
+        #lr = LogisticRegression(maxIter=10, regParam=0.3, elasticNetParam=0.8)
+        #nb = NaiveBayes(smoothing=1.0, modelType="multinomial")
+        lsvc = LinearSVC(maxIter=10, regParam=0.1)
 
-    # Create the trainer and set its parameters
-    ml = MultilayerPerceptronClassifier(maxIter=10, layers=layers, blockSize=128, seed=1234, labelCol="label")
+        pipeline = Pipeline(stages=[tokenizer, hashingTF, idf, lsvc])
 
-    print("Prepare Cross Validation ...")
-    # Define the pipeline for training data
-    pipeline = Pipeline(stages=[tokenizer, hashingTF, idf, ml])
+        model = pipeline.fit(trainingData)
 
-    # Define the Param Grid
-    paramGrid = ParamGridBuilder().addGrid(ml.solver, ["gd", "l-bfgs"])\
-        .addGrid(ml.stepSize, [0.03, 0.02, 0.01]).build()
+    else:
+        print("Prepare Multilayer Perceptron ...")
+        layers = [150, 64, 16, 2]
 
-    crossVal = CrossValidator(estimator=pipeline, estimatorParamMaps=paramGrid,
-                              evaluator=MulticlassClassificationEvaluator(),
-                              numFolds=5, parallelism=1)
+        # Create the trainer and set its parameters
+        ml = MultilayerPerceptronClassifier(maxIter=10, layers=layers, blockSize=128, seed=1234, labelCol="label")
 
-    print("Start Cross Validation ...")
-    cvModel = crossVal.fit(trainingData)
+        print("Prepare Cross Validation ...")
+        # Define the pipeline for training data
+        pipeline = Pipeline(stages=[tokenizer, hashingTF, idf, ml])
+
+        # Define the Param Grid
+        paramGrid = ParamGridBuilder().addGrid(ml.solver, ["gd", "l-bfgs"])\
+            .addGrid(ml.stepSize, [0.03, 0.02, 0.01]).build()
+
+        crossVal = CrossValidator(estimator=pipeline, estimatorParamMaps=paramGrid,
+                                  evaluator=MulticlassClassificationEvaluator(),
+                                  numFolds=5, parallelism=1)
+
+        print("Start Cross Validation ...")
+        model = crossVal.fit(trainingData)
 
     print("Compute Accuracy ...")
     # Compute accuracy on the test set
-    result = cvModel.transform(testData)
+    result = model.transform(testData)
     predictionAndLabels = result.select("prediction", "label")
     evaluator = MulticlassClassificationEvaluator(metricName="accuracy")
     print("Test set accuracy = " + str(evaluator.evaluate(predictionAndLabels)))
